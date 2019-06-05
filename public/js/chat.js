4a69b4a432ca94866cf999ab86ac84bc3aa99173
paranerd <paranerd.development@gmail.com>
2019-06-05 20:51:46 +0200

    Spaces to tabs

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,34 @@
 $(function() {
-    var username = $('head').data('username')
-    var socket = io();
+	var username = $('head').data('username')
+	var socket = io();
 
-    $('form').submit(function(){
-        socket.emit('chat message', $('#msg').val());
-        addMessage($("#msg").val(), username, 'own');
-        $('#msg').val('');
-        return false;
-    });
+	$('form').submit(function(){
+		socket.emit('chat message', $('#msg').val());
+		addMessage($("#msg").val(), username, 'own');
+		$('#msg').val('');
+		return false;
+	});
 
-    socket.on('system', function (msg) {
-        addMessage(msg, "System", 'system');
-    });
+	socket.on('system', function (msg) {
+		addMessage(msg, "System", 'system');
+	});
 
-    socket.on('broadcast', function (msg) {
-        addMessage(msg, "System", 'broadcast');
-    });
+	socket.on('broadcast', function (msg) {
+		addMessage(msg, "System", 'broadcast');
+	});
 
-    socket.on('chat message', function(msg) {
-        addMessage(msg.msg, msg.username);
-    });
+	socket.on('chat message', function(msg) {
+		addMessage(msg.msg, msg.username);
+	});
 });
 
 function addMessage(msg, username, type) {
-    username = (type == 'own') ? "" : username;
-    $("#messages").append($('<li class="' + type + '">').append($('<div>').append($('<span class="msg-user">').text(username)).append($('<span class="msg-text">').text(msg)).append($('<span class="msg-time">').text(getTime()))));
-    $("#messages").animate({scrollTop: $("#messages").prop('scrollHeight')});
+	username = (type == 'own') ? "" : username;
+	$("#messages").append($('<li class="' + type + '">').append($('<div>').append($('<span class="msg-user">').text(username)).append($('<span class="msg-text">').text(msg)).append($('<span class="msg-time">').text(getTime()))));
+	$("#messages").animate({scrollTop: $("#messages").prop('scrollHeight')});
 }
 
 function getTime() {
-    var d = new Date();
-    return d.getHours() + ":" + d.getMinutes();
+	var d = new Date();
+	return d.getHours() + ":" + d.getMinutes();
 }
