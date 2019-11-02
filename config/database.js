787f4f90dfab080a5b5332a725d69d789528129c
paranerd <paranerd.development@gmail.com>
2019-11-02 09:09:35 +0100

    Added recommended parameters to Mongoose constructor

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect(`mongodb://127.0.0.1:27017/chat`);
+mongoose.connect(`mongodb://127.0.0.1:27017/chat`, {useNewUrlParser: true, useUnifiedTopology: true});
 
 console.log(__dirname);
